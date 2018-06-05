afe3f6a44cce07811d9d55058542b03caa56b729
MAC_MINI_6 <codebrew@iqs-mac-2.local>
2018-06-05 15:38:52 +0530

    Initial Commit

---
+++ DynamicAccessoryViewChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicAccessoryViewChat
+//
+//  Created by MAC_MINI_6 on 05/06/18.
+//  Copyright © 2018 MAC_MINI_6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
