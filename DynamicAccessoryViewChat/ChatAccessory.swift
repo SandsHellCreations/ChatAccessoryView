7100277465766c27c5dc7f3a56e0c7c547a2ff8a
MAC_MINI_6 <codebrew@IQs-Mac-2.local>
2018-06-06 12:32:35 +0530

    first commit

---
+++ DynamicAccessoryViewChat/ChatAccessory.swift
@@ -0,0 +1,21 @@
+//
+//  ChatAccessory.swift
+//  DynamicAccessoryViewChat
+//
+//  Created by MAC_MINI_6 on 06/06/18.
+//  Copyright © 2018 MAC_MINI_6. All rights reserved.
+//
+
+import UIKit
+
+class ChatAccessory: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DynamicAccessoryViewChat/ChatCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChatCell.swift
+//  DynamicAccessoryViewChat
+//
+//  Created by MAC_MINI_6 on 06/06/18.
+//  Copyright © 2018 MAC_MINI_6. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
